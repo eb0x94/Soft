12be313901aa11bf4df19dc317798c9b0670f159
eb0x94 <megapr0n94@gmail.com>
2016-12-02 00:22:54 +0200

    Added create form for a message.

--- src/Views/CreateMessageView.js
+++ src/Views/CreateMessageView.js
@@ -3,9 +3,30 @@ import React, { Component } from 'react';
 export default class CreateMessageView extends Component{
     render(){
         return(
-            <div>
-                Create message is under construction.
-            </div>
+            <form className="create-message-form" onSubmit={this.submitForm.bind(this)}>
+                <h1>Create Message for your entrance</h1>
+                <label>
+                    <div>Title:</div>
+                    <input type="text" name="title" required
+                           ref={e => this.titleField = e} />
+                </label>
+                <label>
+                    <div>Description:</div>
+                    <textarea name="description" rows="10"
+                              ref={e => this.descriptionField = e} />
+                </label>
+                <div>
+                    <input type="submit" value="Create" />
+                </div>
+            </form>
+        );
+    }
+
+    submitForm(event) {
+        event.preventDefault();
+        this.props.onsubmit(
+            this.titleField.value,
+            this.descriptionField.value,
         );
     }
 }
